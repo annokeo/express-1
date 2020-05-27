5bd1ce9b2995d37ce2aa33d20dfc8c2cc8a6e244
Anthony T Nokeo <55857508+annokeo@users.noreply.github.com>
2020-05-27 11:06:31 -0700

    Update server.js

--- server.js
+++ server.js
@@ -7,14 +7,10 @@ const PORT = 3000;
 //set views to ejs
 app.set('view engine','ejs');
 
-//we can now serve up static pages
-//app.use(express.static(__dirname));
-
 //this is how a route is built in Express
 app.get('/',(req,res)=>{
-  //res.send(`App is running on port ${PORT}`); 
-  //res.json(data);
-  res.render('pages/index');
+  var title = "Our Home Page";
+  res.render('pages/index',{title:title});
 });
 
 app.listen(PORT,()=>{
